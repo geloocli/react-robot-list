de21c8cdb9460075cffe3f50f7e570055a4cd58b
geloocli <geloocli@gmail.com>
2020-03-16 12:44:24 +0800

    Updates

---
+++ precache-manifest.2e37c2bf6399707fc88405efe4746e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30709b184f248ddd2620a31ebe131bb3",
+    "url": "/react-robot-list/index.html"
+  },
+  {
+    "revision": "d9b9cec29090b294eded",
+    "url": "/react-robot-list/static/css/main.afb04e5c.chunk.css"
+  },
+  {
+    "revision": "a7bed8c2f8b0978ccb20",
+    "url": "/react-robot-list/static/js/2.519cde0a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-robot-list/static/js/2.519cde0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b9cec29090b294eded",
+    "url": "/react-robot-list/static/js/main.034f9f13.chunk.js"
+  },
+  {
+    "revision": "2f102375e481e4ba2e00",
+    "url": "/react-robot-list/static/js/runtime-main.782a0737.js"
+  }
+]);
